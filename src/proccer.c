44bfdd6ae23d1e27132dfe1f8d629996c27c37a2
Dan Molik <dan@d3fy.net>
2016-04-05 16:24:23 -0400

    cleanup and more stat definitions

--- src/proccer.c
+++ src/proccer.c
@@ -6,7 +6,18 @@ static char * _pfile(int pid, char *file)
 	sprintf(tmp, "/proc/%d/%s", pid, file);
 	return tmp;
 }
+char * _trim(char *str)
+{
+	char *end;
+	while (isspace(*str)) str++;
+	if (*str == 0) return str;
 
+	end = str + strlen(str) - 1;
+	while (end > str && isspace(*end)) end--;
+
+	*(end + 1) = 0;
+	return str;
+}
 /*
 process_ pull_stats(int pid)
 {
@@ -22,14 +33,15 @@ int main (void)
 	struct dirent *ep;
 
 	int count = 0;
-	dp = opendir ("/proc");
-	if (dp != NULL) {
+	if ((dp = opendir ("/proc")) != NULL) {
 		while ((ep = readdir (dp)))
 			if (strspn(ep->d_name, "0123456789") == strlen(ep->d_name))
 				pids[count++] = atoi(ep->d_name);
 		(void) closedir (dp);
-	} else
-		perror("Couldn't open /proc dir\n");
+	} else {
+		fprintf(stderr, "failed to /proc %s\n", strerror(errno));
+		exit(1);
+	}
 
 	for (int i = 0; i < count; i++) {
 		if (pids[i] == 2 || pids[i] == 1) continue;
@@ -38,31 +50,34 @@ int main (void)
 			fprintf(stderr, "failed to open stat file for pid %d; %s\n", pids[i], strerror(errno));
 			exit(1);
 		}
-		int ppid;
-		int pid;
 		char *tcomm = malloc(128);
 		char state;
-		fscanf(io, "%d %s %c %d ", &pid, tcomm, &state, &ppid);
-		fclose(io);
-		if (ppid == 2) continue;
 		process_ process;
-		process.pid  = pid;
-		process.ppid = ppid;
+		if (fscanf(io, "%u %s %c %u ", &process.pid, tcomm, &state, &process.ppid) == 0) {
+			fprintf(stderr, "failed to read stat file for pid %u: %s\n", process.pid, strerror(errno));
+			exit(1);
+		}
+		fclose(io);
+		if (process.ppid == 2) continue;
 		if (!(io = fopen(_pfile(pids[i], "cmdline"), "r"))) {
-			fprintf(stderr, "failed to open cmdline file for pid %d; %s\n", pids[i], strerror(errno));
+			fprintf(stderr, "failed to open cmdline file for pid %d; %s\n", process.pid, strerror(errno));
 			exit(1);
 		}
 		size_t len = 0;
 		char *path;
-		getline(&path, &len, io);
+		if (getline(&path, &len, io) == -1) {
+			fclose(io);
+			fprintf(stderr, "failed to read cmdline file for pid %d; %s\n", process.pid, strerror(errno));
+			exit(1);
+		}
 		fclose(io);
 		for (unsigned int j = 0; j < len - 1; j++)
 			if (path[j] == '\0')
 				path[j] = ' ';
-		path[len - 1] = '\0';
-		process.cmd = path;
-		printf("pid [%d] cmd [%s]\n", process.pid, process.cmd);
-
+		path[len - 1] = 0;
+		process.cmd = _trim(path);
+		// @TEST
+		// printf("pid [%u] ppid [%u] cmd [%s]\n", process.pid, process.ppid, process.cmd);
 	}
 
 	return 0;
--- src/proccer.h
+++ src/proccer.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2016 Dan Molik <dan@d3fy.net>
+ *
+ * This file is part of Proccer
+ *
+ * Shitty Little Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Shitty Little Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with Proccer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PROCCER_H
+#define _PROCCER_H
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -7,18 +29,31 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <pthread.h>
 
-#ifndef _PROCCER_H
-#define _PROCCER_H
 
 #define MAX_PIDS 1024
 
+
 typedef struct {
-	unsigned long long utime;
-	unsigned long long stime;
-	unsigned long long cutime;
-	unsigned long long cstime;
+	unsigned long long  utime;
+	unsigned long long  stime;
+	unsigned long long  cutime;
+	unsigned long long  cstime;
+	unsigned long long  gtime;
+	unsigned long long  itime;
+
+	unsigned long       threads;
+	unsigned int        procs;
+	unsigned long       fds;
+
+	unsigned long       vmpeak;
+	unsigned long       vmsize;
+	unsigned long       vmrss;
+	unsigned long       vmhwm;
+
 } stats_;
+
 
 typedef struct {
 	unsigned int  pid;
